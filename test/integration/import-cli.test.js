37971fe8ba1614c7f47adaf3e508fe55918dca98
Benedikt Rötsch <benedikt@contentful.com>
2018-02-01 17:23:42 +0100

    fixup! fixup! test(runner): replace tape with jest

--- test/integration/import-cli.test.js
+++ test/integration/import-cli.test.js
@@ -12,7 +12,7 @@ const managementToken = process.env.MANAGEMENT_TOKEN
 const orgId = process.env.ORG_ID
 let spaceHandle
 
-jest.setTimeout(60000)
+jest.setTimeout(1.5 * 60 * 1000) // 1.5min timeout
 
 afterAll(() => {
   return spaceHandle.delete()
@@ -22,21 +22,25 @@ test('It should import space properly when running as a cli', (done) => {
   const client = createClient({accessToken: managementToken})
   client.createSpace({name: 'temp contentful-import space'}, orgId)
     .then((space) => {
+      console.log(`Created temporary space ${space.sys.id} to test importing via CLI`)
       spaceHandle = space
       app()
         .run(` --space-id ${space.sys.id} --management-token ${managementToken} --content-file ${resolve(__dirname, 'sample-space.json')}`)
-        .stdout(/The following entities are going to be imported/)
-        .stdout(/Content Types {13}| 3/)
-        .stdout(/Editor Interfaces {9}| 3/)
-        .stdout(/Entries {19}| 6/)
-        .stdout(/Assets {20}| 6/)
-        .stdout(/Locales {19}| 1/)
-        .stdout(/Webhooks {18}| 0/)
+        .code(0)
         .expect((result) => {
           if (result.stderr.length) {
+            console.log(result.stdout)
+            console.log(result.stderr)
             throw new Error('Should not have stderr output.')
           }
         })
+        .stdout(/The following entities are going to be imported:/)
+        .stdout(/Content Types +│ 2/)
+        .stdout(/Editor Interfaces +│ 2/)
+        .stdout(/Entries +│ 4/)
+        .stdout(/Assets +│ 4/)
+        .stdout(/Locales +│ 1/)
+        .stdout(/Webhooks +│ 0/)
         .end((error) => {
           expect(error).toBe(undefined)
           done()
--- test/integration/import-lib.test.js
+++ test/integration/import-lib.test.js
@@ -10,7 +10,7 @@ const sampleSpaceFile = join(__dirname, 'sample-space.json')
 
 let spaceHandle
 
-jest.setTimeout(60000)
+jest.setTimeout(1.5 * 60 * 1000) // 1.5min timeout
 
 afterAll(() => {
   return spaceHandle.delete()
@@ -20,6 +20,7 @@ test('It should import a space properly when used as a lib', () => {
   const client = createClient({ accessToken: managementToken })
   return client.createSpace({name: 'temp contentful-import space'}, orgId)
     .then((space) => {
+      console.log(`Created temporary space ${space.sys.id} to test importing as lib`)
       spaceHandle = space
       return runContentfulImport({
         spaceId: space.sys.id,
@@ -28,7 +29,22 @@ test('It should import a space properly when used as a lib', () => {
         useVerboseRenderer: true
       })
         .catch((multierror) => {
-          const failedPublishErrors = multierror.errors.filter((error) => error.hasOwnProperty('error') && error.error.message.indexOf('Could not publish the following entities') !== -1)
+          // only fail on relevant errors
+          const failedPublishErrors = multierror.errors.filter((error) => {
+            if (!error.hasOwnProperty('error')) {
+              return false
+            }
+
+            if (typeof error.error === 'string' && error.error.indexOf('Could not publish the following entities') !== -1) {
+              return false
+            }
+
+            if ('message' in error.error && error.error.message === 'Asset is taking longer then expected to process') {
+              return false
+            }
+
+            return true
+          })
           expect(failedPublishErrors).toHaveLength(0)
           return space.delete()
         })
